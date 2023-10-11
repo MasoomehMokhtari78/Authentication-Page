2cd235917563f62e517d563f14d767bf60e7db7c
masoomehmokhtari78 <masoomehmokhtari693@gmail.com>
2023-10-11 22:31:38 +0330

    database railway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Bb38SHn3rprFPP1yCMh0',
+        'HOST': 'containers-us-west-144.railway.app',
+        'PORT': '7534',
     }
 }
 
