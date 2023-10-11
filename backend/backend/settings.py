4330e30558012b0df74e9bc98a780102edab1bbc
masoomehmokhtari78 <masoomehmokhtari693@gmail.com>
2023-10-11 16:32:13 +0330

    vercel link check 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'https://authentication-page-1nh6.vercel.app',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'https://authentication-page-1nh6.vercel.app',
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
